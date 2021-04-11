e5099453a204ebba89ca31b32370132546c02230
zhoufengfan <zhfengfan@126.com>
2021-04-11 09:43:25 +0800

    edit conv1d

--- conv1d_.py
+++ conv1d_.py
@@ -2,7 +2,12 @@ import torch
 import torch.nn as nn
 
 if __name__ == '__main__':
-    m = nn.Conv1d(in_channels=16, out_channels=33, kernel_size=3, stride=2)
-    input = torch.randn(20, 16, 50)
+    # m = nn.Conv1d(in_channels=16, out_channels=33, kernel_size=3, stride=2)
+    # out_channels can be set to any int value(if not overflow).
+    m = nn.Conv1d(in_channels=1, out_channels=10000, kernel_size=3, stride=1)
+    # [batch_size, in_channels, $L_{in}$]
+    # input = torch.randn(20, 16, 50)
+    input = torch.randn(20, 1, 5)
+    print("input.shape is", input.shape)
     output = m(input)
     print("output.shape is", output.shape)
